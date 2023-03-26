6c52dffae61f5434d99b4555ee694e896079b741
AdamRFaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-03-26 21:04:26 +0700

    menambahkan class SayaTubeVideo

--- TpModul6_1302210008/TpModul6_1302210008/Program.cs
+++ TpModul6_1302210008/TpModul6_1302210008/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
 
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        id = random.Next(10000, 100000);
+        this.title = title;
+        playCount = 0;
+
+    }
+
+    public void increasePlayCount(int playCount)
+    {
+        this.playCount += playCount;
+    }
+
+    public void printVideoDetails()
+    {
+        Console.WriteLine("Judul Video: " + "[" + id + "]" + title);
+        Console.WriteLine("Views: " + playCount);
+    }
+}
+
+class Utama
+{
+    public static void Main()
+    {
+        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract – [ADAM RAFIF FAQIH]");
+        sayaTubeVideo.increasePlayCount(1000);
+        sayaTubeVideo.printVideoDetails();
+    }
+}
