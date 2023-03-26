00c9a66e3782b2ee0db89875f2f10db8effa16c0
AdamRFaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-03-26 23:33:23 +0700

    implementasi design by contract

--- TpModul6_1302210008/TpModul6_1302210008/Program.cs
+++ TpModul6_1302210008/TpModul6_1302210008/Program.cs
@@ -1,27 +1,61 @@
-﻿class SayaTubeVideo
+﻿using System.Diagnostics;
+
+class SayaTubeVideo
 {
     private int id;
     private string title;
     private int playCount;
 
+
     public SayaTubeVideo(string title)
     {
+        Debug.Assert(title != "","JUDUL TIDAK BOLEH KOSONG");
+        Debug.Assert(title.Length <= 100, "JUDUL TIDAK BOLEH LEBIH DARI 100 KARAKTER");
+        this.isStringNull(title);
         Random random = new Random();
-        id = random.Next(10000, 100000);
+        this.id = random.Next(10000, 100000);
         this.title = title;
-        playCount = 0;
-
+        this.playCount = 0;
+        this.isStringNull(this.title);
     }
 
     public void increasePlayCount(int playCount)
     {
-        this.playCount += playCount;
+        Debug.Assert(playCount <= 10000000, "INPUT TIDAK BOLEH LEBIH DARI 10 JUTA");
+        //this.playCount += playCount;
+        
+        checked
+        {
+                this.playCount += playCount;
+        }
+        isIntegerOverflow(this.playCount);
     }
 
     public void printVideoDetails()
     {
-        Console.WriteLine("Judul Video: " + "[" + id + "]" + title);
-        Console.WriteLine("Views: " + playCount);
+        Console.WriteLine("Judul Video: " + "[" + this.id + "]" + this.title);
+        Console.WriteLine("Views: " + this.playCount);
+    }
+
+    private void isStringNull(string title)
+    {
+        if (String.IsNullOrEmpty(title))
+        {
+            throw new ArgumentNullException("INPUT JANGAN NULL");
+        }
+
+        if (String.IsNullOrWhiteSpace(title))
+        {
+            throw new ArgumentNullException("INPUT JANGAN WHITESPACE");
+        }
+    }
+
+    private void isIntegerOverflow(int masuk)
+    {
+        if (masuk >= int.MaxValue || masuk <= int.MinValue)
+        {
+            throw new OverflowException("PENUH");
+        }
     }
 }
 
@@ -29,8 +63,17 @@ class Utama
 {
     public static void Main()
     {
-        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract – [ADAM RAFIF FAQIH]");
-        sayaTubeVideo.increasePlayCount(1000);
-        sayaTubeVideo.printVideoDetails();
+        try
+        {
+            SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract – [ADAM RAFIF FAQIH]");
+            int a = int.MaxValue;
+            sayaTubeVideo.increasePlayCount(a);
+
+            sayaTubeVideo.printVideoDetails();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
